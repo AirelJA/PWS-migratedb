6a3edc841208b16c74fecb2686af823887c6fc90
AirelJavier <airel.javier.ft20@mail.umy.ac.id>
2022-10-19 00:14:52 +0700

    first commit

---
+++ src/main/java/ws/b/kelompok_8/Kelompok8Application.java
@@ -0,0 +1,13 @@
+package ws.b.kelompok_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kelompok8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kelompok8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/b/kelompok_8/Kelompok8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.b.kelompok_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kelompok8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
